6a14e95738c239b8d8e9376c8078fb5ee53980b9
F23413 <sukona21@osu.cz>
2026-02-09 14:02:59 +0100

    Text v Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,6 @@ void main() {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         IO.println("i = " + i);
+        //nová verze se jmenuje master
     }
 }
