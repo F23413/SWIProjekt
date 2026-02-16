5dc0ac2242fce0ec3497cbe7d8da6dbe61939b33
F23413 <sukona21@osu.cz>
2026-02-16 11:26:34 +0100

    Přidáno ddl tabulky

---
+++ src/DDLTabulka.sql
@@ -0,0 +1,8 @@
+-- auto-generated definition
+create table tableone
+(
+    PersonID  int          not null
+        primary key,
+    LastName  varchar(255) null,
+    FirstName varchar(255) null
+);
