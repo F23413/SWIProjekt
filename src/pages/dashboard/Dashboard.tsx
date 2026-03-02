44b812759d6236086b0f2bdcdb4364db5f7ce5aa
F23413 <sukona21@osu.cz>
2026-03-02 20:48:26 +0100

    Tabulka zaměstnanců, postování zaměstnanců

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,6 +1,65 @@
+import {useEffect, useState} from "react";
+import {Button, Col, Container, Row, Table} from "react-bootstrap";
+
+type Employee = {
+    id: number;
+    name: string;
+    email: string;
+    phone: string;
+    department: string;
+};
+
 const Dashboard = () => {
+
+    const [employees, setEmployees] = useState<Employee[]>([]);
+
+    useEffect(() => {
+        const fetchEmployees = async () =>{
+            try{
+                const response = await fetch("http://localhost:8080/api/employees");
+                const data = await response.json();
+
+                setEmployees(data);
+            }catch(error){
+                console.error("Chyba při hledání zaměstnanců: ", error);
+            }
+        }
+        fetchEmployees();
+    }, [])
+
     return (
-        <div>Nástěnka</div>
+        <>
+            <Container className="mt-5">
+                <Row className="justify-content-center">
+                    <Col className="text-center">
+                        <h1 className="mb-5">Zaměstnanci</h1>
+                        <Table striped bordered hover responsive>
+                            <thead>
+                            <tr>
+                                <th scope="col">Jméno</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Tel. číslo</th>
+                                <th scope="col">Oddělení</th>
+                                <th scope="col">Činnost</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {employees.map((employee) => (
+                                <tr key={employee.id}>
+                                    <td>{employee.name}</td>
+                                    <td>{employee.email}</td>
+                                    <td>{employee.phone}</td>
+                                    <td>{employee.department}</td>
+                                    <Button variant="outline-primary">Uložit</Button>{" "}
+                                    <Button variant="outline-danger">Odstranit</Button>
+                                </tr>
+                            ))}
+                            </tbody>
+                        </Table>
+                    </Col>
+                </Row>
+            </Container>
+        </>
     )
 }
 export default Dashboard;
--- src/pages/employee/PostUser.tsx
+++ src/pages/employee/PostUser.tsx
@@ -1,6 +1,7 @@
 import "./PostUser.css"
 import {useState} from "react";
 import {Button, Form} from "react-bootstrap";
+//import {useNavigate} from "react-router-dom";
 
 const PostUser = () => {
     const [formData, setFormData] = useState({
@@ -18,24 +19,60 @@ const PostUser = () => {
         })
     }
 
+    //const navigate = useNavigate();
+
+    const handleSubmit = async (
+        e: React.FormEvent<HTMLFormElement>
+    ) => {
+        e.preventDefault();
+        console.log(formData);
+        /*try {
+            const response = await fetch("http://localhost:8080/api/employee", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(formData)
+            });
+
+            if (!response.ok) {
+                throw new Error("Chyba při ukládání uživatele");
+            }
+
+            const data = await response.json();
+            console.log("Uživatel uložen: ", data);
+            navigate("/");
+
+        } catch (error) {
+            console.error("Došlo k chybě: ", error.message);
+        }*/
+    };
+
+
     return(
         <>
+            <div className="PostUser">
 <div className="center-form">
-    <div className="bg-dark">
-        <h1 className=" text-danger-emphasis">Přidání nového zaměstnance</h1>
+    <div className="card">
+        <div className="card-header">
+            <h3 className=" text-primary-emphasis">Přidání nového zaměstnance</h3>
+        </div>
+        <div className="card-body">
+            <Form onSubmit={handleSubmit}>
+                <Form.Group controlId="formBasicName">
+                    <Form.Control type="text" name="name" placeholder="Jméno" value={formData.name} onChange={handleInputChange}></Form.Control>
+                    <Form.Control type="email" name="email" placeholder="Email" value={formData.email} onChange={handleInputChange}></Form.Control>
+                    <Form.Control type="number" name="phone" placeholder="Tel. číslo" value={formData.phone} onChange={handleInputChange}></Form.Control>
+                    <Form.Control type="text" name="department" placeholder="Oddělení" value={formData.department} onChange={handleInputChange}></Form.Control>
+                </Form.Group>
+                <Button variant="primary" type="submit" className="w-100">
+                    Uložit nového zaměstnance
+                </Button>
+            </Form>
+        </div>
     </div>
-    <Form>
-        <Form.Group controlId="formBasicName">
-            <Form.Control type="text" name="name" placeholder="Jméno" value={formData.name} onChange={handleInputChange}></Form.Control>
-            <Form.Control type="email" name="email" placeholder="Email" value={formData.email} onChange={handleInputChange}></Form.Control>
-            <Form.Control type="text" name="phone" placeholder="Tel. číslo" value={formData.phone} onChange={handleInputChange}></Form.Control>
-            <Form.Control type="text" name="department" placeholder="Oddělení" value={formData.department} onChange={handleInputChange}></Form.Control>
-        </Form.Group>
-        <Button variant="primary" type="submit" className="w-100">
-            Uložit nového zaměstnance
-        </Button>
-    </Form>
 </div>
+            </div>
         </>
     )
 }
