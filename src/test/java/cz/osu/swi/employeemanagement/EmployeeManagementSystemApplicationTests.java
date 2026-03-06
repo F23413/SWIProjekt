a19920d4313eae32ae43ce28ce59a101a9af6740
Vilém Žamboch <vilemzamboch.official@seznam.cz>
2026-03-06 21:07:58 +0100

    Initial Spring Boot backend setup
    - Spring Boot project initialized
    - Database connection configured
    - data.sql initialization

---
+++ src/test/java/cz/osu/swi/employeemanagement/EmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.osu.swi.employeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
