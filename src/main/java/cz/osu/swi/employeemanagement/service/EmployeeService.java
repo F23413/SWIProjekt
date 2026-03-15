fdf764b0770aa76bf87207f75202a9b0e1a05ea3
Vilém Žamboch <vilemzamboch.official@seznam.cz>
2026-03-15 22:31:27 +0100

    Added to EmployeeService.java - getEmployeeByID method - updateEmployee method - deleteEmployee method

--- src/main/java/cz/osu/swi/employeemanagement/service/EmployeeService.java
+++ src/main/java/cz/osu/swi/employeemanagement/service/EmployeeService.java
@@ -2,10 +2,12 @@ package cz.osu.swi.employeemanagement.service;
 
 import cz.osu.swi.employeemanagement.entity.Employee;
 import cz.osu.swi.employeemanagement.repository.EmployeeRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +21,33 @@ public class EmployeeService {
 
     public List<Employee> getEmployees() {
         return employeeRepository.findAll();
+    }
+
+    public void deleteEmployee(Long id) {
+        if(!employeeRepository.existsById(id)){
+            throw new EntityNotFoundException("Employee with id " + id + " not found");
+        }
+
+        employeeRepository.deleteById(id);
+    }
+
+    public Employee getEmployeeByID(Long id) {
+
+        return employeeRepository.findById(id).orElse(null);
+    }
+
+    public Employee updateEmployee(Long id, Employee employee) {
+        Optional<Employee> employeeOptional = employeeRepository.findById(id);
+        if(employeeOptional.isPresent()){
+            Employee existingEmployee = employeeOptional.get();
+            existingEmployee.setEmail(employee.getEmail());
+            existingEmployee.setName(employee.getName());
+            existingEmployee.setPhone(employee.getPhone());
+            existingEmployee.setDepartment(employee.getDepartment());
+
+            return employeeRepository.save(existingEmployee);
+        }
+        return null;
     }
 
 }
