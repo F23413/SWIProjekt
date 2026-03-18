2e933151a7e8590c02b08b19856dab2cf53c3eeb
F23413 <sukona21@osu.cz>
2026-03-18 18:58:58 +0100

    MySQL úpravy

--- src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-TRUNCATE TABLE employee;
-
-INSERT INTO employee (name, email, phone, department) VALUES
-                                                          ('Jan Novák', 'jan.novak@example.com', '+420123456789', 'IT'),
-                                                          ('Petra Svobodová', 'petra.svobodova@example.com', '+420987654321', 'HR'),
-                                                          ('Tomáš Dvořák', 'tomas.dvorak@example.com', '+420111222333', 'Finance'),
-                                                          ('Lucie Černá', 'lucie.cerna@example.com', '+420444555666', 'Marketing'),
-                                                          ('Martin Procházka', 'martin.prochazka@example.com', '+420777888999', 'IT');
